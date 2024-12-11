9296f3604c609728626754204d7a6a7823597cf4
marianicolepaco <mariaalexandranicole.paco@edu.sait.ca>
2024-12-10 17:20:24 -0700

    Project finished

---
+++ app/quiz-data.js
@@ -0,0 +1,280 @@
+const QuizData = {
+  intro: [
+    {
+      questionText: "Who is considered the central figure of Christianity?",
+      answerOptions: [
+        { answerText: "Allah", isCorrect: false },
+        { answerText: "Jesus Christ", isCorrect: true },
+        { answerText: "Moses", isCorrect: false },
+        { answerText: "Abraham", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Who were the first two people created by God?",
+      answerOptions: [
+        { answerText: "Noah and his wife", isCorrect: false },
+        { answerText: "Adam and Eve", isCorrect: true },
+        { answerText: "Cain and Abel", isCorrect: false },
+        { answerText: "Abraham and Sarah", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What is the most important holiday for Christians?",
+      answerOptions: [
+        { answerText: "Thanksgiving Day", isCorrect: false },
+        { answerText: "Halloween", isCorrect: false },
+        { answerText: "Remembrance Day", isCorrect: false },
+        { answerText: "Christmas", isCorrect: true }
+      ]
+    },
+    {
+      questionText: "What is the symbol of Christianity?",
+      answerOptions: [
+        { answerText: "The Ankh", isCorrect: false },
+        { answerText: "Om", isCorrect: false },
+        { answerText: "Cross", isCorrect: true },
+        { answerText: "Star of David", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What is the act of communicating to God called?",
+      answerOptions: [
+        { answerText: "Yoga", isCorrect: false },
+        { answerText: "Meditation", isCorrect: false },
+        { answerText: "Prayer", isCorrect: true },
+        { answerText: "Manifestation", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What is the main place of worship for Christians?",
+      answerOptions: [
+        { answerText: "Church", isCorrect: true },
+        { answerText: "Temple", isCorrect: false },
+        { answerText: "Synagogue", isCorrect: false },
+        { answerText: "Mosque", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What is the Holy Book of Christianity?",
+      answerOptions: [
+        { answerText: "The Torah", isCorrect: false },
+        { answerText: "The Quran", isCorrect: false },
+        { answerText: "The Vedas", isCorrect: false },
+        { answerText: "The Bible", isCorrect: true }
+      ]
+    },
+    {
+      questionText: "What is the central message of Christianity?",
+      answerOptions: [
+        { answerText: "Believe in Tarot Cards", isCorrect: false },
+        { answerText: "Love your neighbor as yourself", isCorrect: true },
+        { answerText: "Worship multiple Gods", isCorrect: false },
+        { answerText: "Hate your enemy", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What was the first thing God created?",
+      answerOptions: [
+        { answerText: "Humans", isCorrect: false },
+        { answerText: "Animals", isCorrect: false },
+        { answerText: "Light", isCorrect: true },
+        { answerText: "Nature", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What is the last word in the Bible?",
+      answerOptions: [
+        { answerText: "Amen.", isCorrect: true },
+        { answerText: "Soon.", isCorrect: false },
+        { answerText: "Love.", isCorrect: false },
+        { answerText: "Jesus.", isCorrect: false }
+      ]
+    }
+  ],
+
+  old: [
+    {
+      questionText: "What did God use to create Eve?",
+      answerOptions: [
+        { answerText: "Tree roots", isCorrect: false },
+        { answerText: "Air/Wind", isCorrect: false },
+        { answerText: "Adam's rib", isCorrect: true },
+        { answerText: "Dust", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What was the name of the first book of the Bible?",
+      answerOptions: [
+        { answerText: "Genesis", isCorrect: true },
+        { answerText: "Exodus", isCorrect: false },
+        { answerText: "Initium", isCorrect: false },
+        { answerText: "Deuteronomy", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Who were the ten commandments given to?",
+      answerOptions: [
+        { answerText: "Jesus", isCorrect: false },
+        { answerText: "Adam", isCorrect: false },
+        { answerText: "John", isCorrect: false },
+        { answerText: "Moses", isCorrect: true }
+      ]
+    },
+    {
+      questionText: "What is the symbol of God's promise to Noah? ",
+      answerOptions: [
+        { answerText: "Rainbow", isCorrect: true },
+        { answerText: "Flood", isCorrect: false },
+        { answerText: "Ark", isCorrect: false },
+        { answerText: "Animals", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Who fought the giant, Goliath?",
+      answerOptions: [
+        { answerText: "Adam", isCorrect: false },
+        { answerText: "David", isCorrect: true },
+        { answerText: "Judas", isCorrect: false },
+        { answerText: "Jesus", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Who was the first murderer in the Bible?",
+      answerOptions: [
+        { answerText: "Abel", isCorrect: false },
+        { answerText: "Noah", isCorrect: false },
+        { answerText: "Cain", isCorrect: true },
+        { answerText: "Adam", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Who did God promise to make the father of many nations?",
+      answerOptions: [
+        { answerText: "Aaron", isCorrect: false },
+        { answerText: "Isaac", isCorrect: false },
+        { answerText: "David", isCorrect: false },
+        { answerText: "Abraham", isCorrect: true }
+      ]
+    },
+    {
+      questionText: "What did Moses part to escape from Egypt?",
+      answerOptions: [
+        { answerText: "Ocean", isCorrect: false },
+        { answerText: "Sea", isCorrect: true },
+        { answerText: "River", isCorrect: false },
+        { answerText: "Mountain", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "When Daniel prayed to God after it was not allowed by the King, where was he thrown into? ",
+      answerOptions: [
+        { answerText: "Wolf's lair", isCorrect: false },
+        { answerText: "Serpent's pit", isCorrect: false },
+        { answerText: "Lion's den", isCorrect: true },
+        { answerText: "Bear's cage", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Which brother of Moses became the first high priest?",
+      answerOptions: [
+        { answerText: "Aaron", isCorrect: true },
+        { answerText: "Joseph", isCorrect: false },
+        { answerText: "John", isCorrect: false },
+        { answerText: "Matthew", isCorrect: false }
+      ]
+    }
+  ],
+
+  new: [
+    {
+      questionText: "Who was the mother of Jesus?",
+      answerOptions: [
+        { answerText: "Sarah", isCorrect: false },
+        { answerText: "Eve", isCorrect: false },
+        { answerText: "Mary", isCorrect: true },
+        { answerText: "Elizabeth", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What is the meaning of the phrase that Jesus said to 'turn the other cheek?'",
+      answerOptions: [
+        { answerText: "Turn your back and leave", isCorrect: false },
+        { answerText: "Forgive and not retaliate against those who wrong us", isCorrect: true },
+        { answerText: "To ignore the problem and hope it goes away", isCorrect: false },
+        { answerText: "Seek revenge at a later time", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "How many disciples did Jesus have?",
+      answerOptions: [
+        { answerText: "10", isCorrect: false },
+        { answerText: "12", isCorrect: true },
+        { answerText: "8", isCorrect: false },
+        { answerText: "15", isCorrect: false }
+      ]
+    },    {
+      questionText: "What was Jesus' teaching on loving your enemies?",
+      answerOptions: [
+        { answerText: "Ignore your enemies", isCorrect: false },
+        { answerText: "Hate those who hate you", isCorrect: false },
+        { answerText: "Love your enemies and pray for those who persecute you", isCorrect: true },
+        { answerText: "Revenge and fight your enemies back", isCorrect: false }
+      ]
+    },    {
+      questionText: "What was the name of the angel who announced to Mary that she would give birth to Jesus?",
+      answerOptions: [
+        { answerText: "Gabriel", isCorrect: true },
+        { answerText: "Uriel", isCorrect: false },
+        { answerText: "Michael", isCorrect: false },
+        { answerText: "Raphael", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What is the shortest verse in the Bible?",
+      answerOptions: [
+        { answerText: "'It is finished.'", isCorrect: false },
+        { answerText: "'Do not be afraid.'", isCorrect: false },
+        { answerText: "'Jesus was born in Bethlehem.'", isCorrect: false },
+        { answerText: "'Jesus wept.'", isCorrect: true }
+      ]
+    },
+    {
+      questionText: "Who is the first disciple to betray Jesus?",
+      answerOptions: [
+        { answerText: "Peter", isCorrect: false },
+        { answerText: "Joseph", isCorrect: false },
+        { answerText: "Judas", isCorrect: true },
+        { answerText: "Paul", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "How is love best described in the Bible?",
+      answerOptions: [
+        { answerText: "Love is patient and kind, it doesn't envy or boast... ", isCorrect: true },
+        { answerText: "Love is a fleeting feeling that fades with time ", isCorrect: false },
+        { answerText: "Love is a spiritual force that guides and protects...", isCorrect: false },
+        { answerText: "Love is a powerful emotion...", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What did the angels proclaim to the sheperds the night Jesus was born?",
+      answerOptions: [
+        { answerText: "'Peace on earth, good will toward men'", isCorrect: false },
+        { answerText: "'Glory to God in the highest'", isCorrect: false },
+        { answerText: "'The Savior has been born'", isCorrect: false },
+        { answerText: "All of the above", isCorrect: true }
+      ]
+    },
+    {
+      questionText: "What is the last book of the New Testament called?",
+      answerOptions: [
+        { answerText: "Psalms", isCorrect: false },
+        { answerText: "Revelation", isCorrect: true },
+        { answerText: "Leviticus", isCorrect: false },
+        { answerText: "Philippians", isCorrect: false }
+      ]
+    }
+  ]
+};
+
+export default QuizData;
---
+++ app/quiz.js
@@ -0,0 +1,147 @@
+"use client";
+
+import React, { useState, useEffect, useCallback } from "react";
+import QuizData from "./quiz-data.js"; 
+import "./styles.css"; 
+
+const Quiz = ({ selectedTopic, onGoBack }) => {
+  const questions = QuizData[selectedTopic];
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [timer, setTimer] = useState(15);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  const handleStartQuiz = () => {
+    setQuizStarted(true);
+    setTimer(15);
+  };
+
+  const handleAnswerOptionClick = useCallback(
+    (isCorrect, answerIndex) => {
+      setSelectedAnswer(answerIndex);
+      if (isCorrect) setScore((prevScore) => prevScore + 1);
+
+      setTimeout(() => {
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+          setCurrentQuestion(nextQuestion);
+          setTimer(15);
+          setSelectedAnswer(null);
+        } else {
+          setShowScore(true);
+        }
+      }, 2000);
+    },
+    [currentQuestion, questions.length]
+  );
+
+  useEffect(() => {
+    if (!quizStarted || showScore || selectedAnswer !== null) return;
+
+    const timerId = setInterval(() => {
+      setTimer((prevTimer) => {
+        if (prevTimer === 1) {
+          clearInterval(timerId);
+          handleAnswerOptionClick(false, null);
+          return 15;
+        }
+        return prevTimer - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, [quizStarted, showScore, selectedAnswer, handleAnswerOptionClick]);
+
+  const handleRetry = () => {
+    setCurrentQuestion(0);
+    setShowScore(false);
+    setScore(0);
+    setTimer(15);
+    setSelectedAnswer(null);
+  };
+
+  const calculateTimerPercentage = () => {
+    return (timer / 15) * 100;
+  };
+
+  return (
+    <div className="app">
+      {!quizStarted ? (
+        <div className="start-section">
+          <h1 className="welcome-text">Welcome to HolyTrivia!</h1>
+          <p className="instructions">Let's see how well you know your Bible. Have fun!</p>
+          <button className="start-button" onClick={handleStartQuiz}>
+            Start Quiz
+          </button>
+        </div>
+      ) : showScore ? (
+        <div className="results-section">
+          <h2 className="score-text">
+            🎉 You scored {score} out of {questions.length}! 🎉
+          </h2>
+          <p>Keep the faith! Let's see if you can beat your last score!</p>
+          <div className="results-buttons">
+            <button className="reset-button" onClick={handleRetry}>
+              Retry
+            </button>
+            <button className="back-button" onClick={onGoBack}>
+              Go Back
+            </button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="timer-bar">
+            <div
+              className="timer-bar-fill"
+              style={{ width: `${calculateTimerPercentage()}%` }}
+            ></div>
+          </div>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span> / {questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-options">
+            {questions[currentQuestion].answerOptions.map(
+              (answerOption, index) => (
+                <button
+                  key={index}
+                  className={`answer-button ${
+                    selectedAnswer !== null &&
+                    (answerOption.isCorrect
+                      ? "correct"
+                      : selectedAnswer === index
+                      ? "wrong"
+                      : "")
+                  }`}
+                  onClick={() =>
+                    selectedAnswer === null &&
+                    handleAnswerOptionClick(answerOption.isCorrect, index)
+                  }
+                  disabled={selectedAnswer !== null}
+                >
+                  {answerOption.answerText}
+                  {selectedAnswer !== null &&
+                    (answerOption.isCorrect
+                      ? " ✔"
+                      : selectedAnswer === index
+                      ? " ✘"
+                      : "")}
+                </button>
+              )
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
+
